ef0b659bd62136136e3a34022ec268862318ddef
john <jonwanjema@gmail.com>
2020-12-21 06:24:28 +0300

    update components

--- resources/js/components/config/usersAdmin.vue
+++ resources/js/components/config/usersAdmin.vue
@@ -7,8 +7,15 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="card">
-                            <div class="card-header">
-                                <strong class="card-title">Admins</strong>
+                            <div class="card-header bg-dark">
+                                 <div class="row">
+                                    <div class="col-md-10">
+                                        <strong class="card-title text-light">Admin</strong>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <button @click="openModal" type="button" class="btn btn-primary">Add Admin</button>
+                                    </div>
+                                </div>
                             </div>
                             <div class="card-body">
                                 <div class="row">
@@ -27,23 +34,68 @@
                                         </div>
                                     </div>
                                     <div class="col-sm-12 col-md-6">
-                                        <b-input-group >
+                                        <b-input-group>
                                             <b-form-input id="filter-input" v-model="filter" type="search" placeholder="Type to Search"></b-form-input>
                                             <b-input-group-append>
-                                                <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+                                                <b-button variant="dark" :disabled="!filter" @click="filter = ''">Clear</b-button>
                                             </b-input-group-append>
                                         </b-input-group>
+                                        <br/>
+                                        <b-button size="sm">Copy</b-button>
+                                        <b-button size="sm">Excel</b-button>
+                                        <b-button size="sm">CSV</b-button>
+                                        <b-button size="sm">PDF</b-button>
                                     </div>
-                                    <!-- <div class="col-sm-12 col-md-6"><br>
-                                        <b-button size="sm">Small Button</b-button>
-                                        <b-button size="sm">Small Button</b-button>
-                                        <b-button size="sm">Small Button</b-button>
-                                    </div> -->
                                 </div><br>
-                                <b-table :items="items" :per-page="perPage" :current-page="currentPage" :fields="fields" :filter="filter" show-empty @filtered="onFiltered"></b-table>
+                                <b-table bordered :items="items" :per-page="perPage" :current-page="currentPage" :fields="fields" :filter="filter" show-empty @filtered="onFiltered">
+                                    <template v-slot:cell(#)="row">
+                                        <p>{{row.index + 1}}</p>
+                                    </template>
+                                    <template v-slot:cell(actions)="row">
+                                        <b-button class="btn btn-sm" variant="success" @click="openEditModal(row.item)">
+                                            Edit
+                                        </b-button>
+                                        &nbsp;
+                                        <b-button class="btn btn-sm" variant="danger">
+                                            Delete
+                                        </b-button>
+                                    </template>
+                                </b-table>
                                 <b-pagination v-model="currentPage" :total-rows="totalRows" :per-page="perPage" aria-controls="my-table"></b-pagination>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="mediumModal" tabindex="-1" role="dialog" aria-labelledby="mediumModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <h5 v-if="!editMode" class="modal-title" id="mediumModalLabel">Add Admin</h5>
+                        <h5 v-else class="modal-title" id="mediumModalLabel">Update Admin</h5>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="company" class="form-control-label">First Name:</label>
+                            <input v-model="form.first_name" type="text"  placeholder="Enter first name" class="form-control" />
+                        </div>
+                        <div class="form-group">
+                            <label  class="form-control-label">Last Name:</label>
+                            <input v-model="form.last_name" type="text" placeholder="Enter last name" class="form-control" />
+                        </div>
+                         <div class="form-group">
+                            <label  class="form-control-label">Phone Number:</label>
+                            <input v-model="form.phone_no" type="text" placeholder="Enter phone number" class="form-control" />
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button v-if="!editMode" type="button" class="btn btn-primary">Add Admin</button>
+                        <button v-else type="button" class="btn btn-primary">Update Admin</button>
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
                     </div>
                 </div>
             </div>
@@ -52,17 +104,24 @@
 </template>
 
 <script>
+import $ from 'jquery';
 export default {
     data() {
         return {
-            fields: ['name', 'role', 'created_at',],
+            fields: ['#','name', 'phone_no','role', 'created_at','actions'],
             items: [
-                {name :'John Doe',role:'admin', created_at:'10/12/2020'}
+                {name :'John Doe',phone_no:'07212739', role:'admin', created_at:'10/12/2020'}
             ],
             perPage: 5,
             currentPage: 1,
             filter: null,
             totalRows: 1,
+             form : new Form({
+                first_name:'',
+                last_name:'',
+                phone_no:'',
+            }),
+            editMode:false,   
         }
     },
     computed: {
@@ -74,6 +133,14 @@ export default {
         onFiltered(filteredItems) {
             this.totalRows = filteredItems.length
             this.currentPage = 1
+        },
+        openModal(){
+            $('#mediumModal').modal('show');
+            this.editMode = false;
+        },
+        openEditModal(student){
+            $('#mediumModal').modal('show');
+            this.editMode = true;
         }
     },
     mounted() {
